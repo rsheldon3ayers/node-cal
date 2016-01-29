db6a6abdeda4517fce6ba9be989c12d01379e05d
Robert Ayers <rsheldon3ayers@gmail.com>
2016-01-29 09:36:54 -0600

    added travis.yml

--- cal.js
+++ cal.js
@@ -1,3 +1,23 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony_destructuring
 'use strict';
-console.log('Hi');
+
+const [,, ...args] = process.argv;
+const center = require('./lib/center.js');
+const getMonth = require('./lib/month.js');
+  console.log(args);
+if (args.length === 2) {
+  const [month, year] = args;
+  console.log(args);
+  let modMonth = getMonth.getMonth(month);
+  center(modMonth, year);
+
+}  if (args.length === 1){
+    const month = args;
+    let modMonth = getMonth.getMonth(month);
+    //console.log("inside cal.js", modMonth);
+    center(modMonth);
+
+}
+
+
+
